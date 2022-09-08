62e4be2486bb91a738c76de4fb226a776b528f0e
Sadiya <Shahidakilgori@gmail.com>
2022-09-08 09:35:33 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
  * main - Entry point
  * Return: 0 after printing the function
  */
-int main(void)                                                  
-	                                                                
+int main(void)
 {puts("\"Programming is like building a multilingual puzzle");  
-	        return (0);    }      ...........................................you have to type it
+	        return (0);    }
