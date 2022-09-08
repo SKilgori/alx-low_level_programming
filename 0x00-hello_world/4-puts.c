23f20034b846e0edd7d97bab14882792255bc1b8
Sadiya <Shahidakilgori@gmail.com>
2022-09-08 09:27:04 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/**
+ * main - Entry point
+ * Return: 0 after printing the function
+ */
+int main(void)                                                  
+	                                                                
+{puts("\"Programming is like building a multilingual puzzle");  
+	        return (0);    }      ...........................................you have to type it
